467dab05f7511ff2470f0ac7b9558ef0079c347d
William <chapman.williamt@gmail.com>
2018-08-04 17:54:19 -0400

    payment class

--- Midterm/Payment.cs
+++ Midterm/Payment.cs
@@ -8,5 +8,54 @@ namespace Midterm
 {
     class Payment
     {
+        #region Constructors
+        public double SubTotal { set; get; }
+        public double SalesTax { set; get; }
+        public double GrandTotal { set; get; }
+        #endregion
+
+        #region Methods
+        public void DisplayReceipt()
+        {
+
+        }
+
+        public string ValidatePaymentType(string userPayment)
+        {
+            if(userPayment.ToLower() == "cash")
+            {
+                return "cash";
+            }
+            else if(userPayment.ToLower() == "check")
+            {
+                return "check";
+            }
+            else if(userPayment.ToLower() == "credit")
+            {
+                return "credit";
+            }
+            else
+            {
+                return "none";
+            }
+        }
+
+        public void CalculateGT()
+        {
+
+        }
+
+        public double CalculateSuTo(int quantity, double itemPrice)
+        {
+            double subTotal = quantity * itemPrice;
+            return subTotal;
+        }
+
+        public double CalculateTax(double subTotal)
+        {
+            double totalTax = subTotal * 0.06d;
+            return totalTax;
+        }
+        #endregion
     }
 }
